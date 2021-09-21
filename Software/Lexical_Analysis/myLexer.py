3a0cb36d15efdc9e593808e433747c45a44ed930
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-09-20 19:40:09 -0600

    #4 [Added] Symbols Table class for Lexical Analysis phase

--- Software/Lexical_Analysis/myLexer.py
+++ Software/Lexical_Analysis/myLexer.py
@@ -1,35 +1,39 @@
 import ply.lex as lex
 import ply.yacc as yacc
 from Software.Syntactic.SyntacticAnalizer import *
+from Software.SymbolsTable import *
 
 
 def lex_test():
     sourceFile = "../Lexical_Analysis/source.txt"
-    if sourceFile is not None:
-                with open(sourceFile, 'r') as file:
-                    print("Entrando al lexer...")
-                    try:
-                        source = file.read()
-                    except EOFError as e:
-                        print(e)
 
-                    lexer = lex.lex()
-                    lexer.input(source)
-                    print("Lexer listo")
-                    print("Entrando al parser...")
-                    parser = yacc.yacc()
-                    print("Saliendo del parser...")
-                    parser.parse(source)
-                    print("TERMINE DE COMPILAR")
+    if sourceFile is not None:
+        with open(sourceFile, 'r') as file:
+            print("Entrando al lexer...")
+            try:
+                source = file.read()
+            except EOFError as e:
+                print(e)
+
+            lexer = lex.lex()
+            lexer.input(source)
+            myTable = SymbolsTable()
+            clone = lexer.clone()
+            clone.input(source)
+
+            for token in clone:
+                myTable.insertToken(token.type, token.value)
+            myTable.printTable()
+
+            parser = yacc.yacc()
+            parser.parse(source)
+            print("Saliendo del parser...")
+            print("TERMINE DE COMPILAR")
 
     else:
         print("ESTA VACIO")
     for Tok in lexer:
         print(Tok)
 
-#lex_test()
 
-
-
-
-
+lex_test()
---
+++ Software/SymbolsTable.py
@@ -0,0 +1,38 @@
+from Software.Lexical_Analysis.Tokenize import *
+import pprint
+
+
+class SymbolsTable:
+    def __init__(self):
+        self.table = {}
+        self.mainCounter = 0
+
+    def insertToken(self, key, name):
+        key = str(key)
+        if key == "ID":
+            self.table[name] = {
+                "type": None,
+                "value": None,
+                "scope": None,
+            }
+
+        elif key == "MAIN":
+            self.mainCounter += 1
+            self.table[name] = {
+                "cantidad": self.mainCounter,
+                "Scope": "Main block",
+            }
+        else:
+            pass
+
+    def printTable(self):
+        pprint.pprint(self.table)
+
+# mySymbolsTable = SymbolsTable()
+# mySymbolsTable.insertToken("var1")
+# mySymbolsTable.insertToken("var2")
+# mySymbolsTable.insertToken("var3")
+# mySymbolsTable.insertToken("var4")
+# mySymbolsTable.insertToken("main")
+# mySymbolsTable.insertToken("main")
+# print(mySymbolsTable.table)
